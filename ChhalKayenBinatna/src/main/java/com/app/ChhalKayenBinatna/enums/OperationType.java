cff84adf1e3a03920a3a832e069d9509cff068d6
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-04-12 14:25:43 +0000

    add enum

---
+++ ChhalKayenBinatna/src/main/java/com/app/ChhalKayenBinatna/enums/OperationType.java
@@ -0,0 +1,9 @@
+package com.app.ChhalKayenBinatna.enums;
+
+public enum OperationType {
+    SEND_MONEY,
+    RESERVE_MONEY,
+    RECHARGER_MONEY,
+    REQUEST_MONEY,
+    OPERATION_DETAIL
+}
---
+++ ChhalKayenBinatna/src/main/java/com/app/ChhalKayenBinatna/enums/Roles.java
@@ -0,0 +1,5 @@
+package com.app.ChhalKayenBinatna.enums;
+
+public enum Roles {
+    ADMIN, USER, GUEST
+}
