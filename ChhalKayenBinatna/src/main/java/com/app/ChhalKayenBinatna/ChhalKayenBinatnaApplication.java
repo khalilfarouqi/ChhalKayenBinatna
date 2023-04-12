941f4e3f58913746e3eab3c14ff2f00f08db86b5
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-04-12 10:15:54 +0000

    initial commit

---
+++ ChhalKayenBinatna/src/main/java/com/app/ChhalKayenBinatna/ChhalKayenBinatnaApplication.java
@@ -0,0 +1,13 @@
+package com.app.ChhalKayenBinatna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChhalKayenBinatnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChhalKayenBinatnaApplication.class, args);
+	}
+
+}
---
+++ ChhalKayenBinatna/src/test/java/com/app/ChhalKayenBinatna/ChhalKayenBinatnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.ChhalKayenBinatna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChhalKayenBinatnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
