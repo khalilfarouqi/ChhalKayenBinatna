f207e35ab2bf2eb7f0d1b7e6935776fab035faab
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-04-19 11:35:32 +0000

    interface Services

---
+++ ChhalKayenBinatna/src/main/java/com/app/ChhalKayenBinatna/service/impl/IBaseService.java
@@ -0,0 +1,12 @@
+package com.app.ChhalKayenBinatna.service.impl;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface IBaseService<E, D extends Serializable> extends IRsqlService<E, D> {
+    D save(D dto);
+    D update(D dto);
+    void delete(Long id);
+    D findById(Long id);
+    List<D> findAll();
+}
