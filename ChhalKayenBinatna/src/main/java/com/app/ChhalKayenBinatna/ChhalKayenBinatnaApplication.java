8fbcc99d0503b8f49f607d4b156009c10b695d5f
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-04-19 12:17:52 +0000

    add modelmapper Bean main application

--- ChhalKayenBinatna/src/main/java/com/app/ChhalKayenBinatna/ChhalKayenBinatnaApplication.java
+++ ChhalKayenBinatna/src/main/java/com/app/ChhalKayenBinatna/ChhalKayenBinatnaApplication.java
@@ -1,7 +1,9 @@
 package com.app.ChhalKayenBinatna;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ChhalKayenBinatnaApplication {
@@ -9,5 +11,8 @@ public class ChhalKayenBinatnaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ChhalKayenBinatnaApplication.class, args);
 	}
-
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
