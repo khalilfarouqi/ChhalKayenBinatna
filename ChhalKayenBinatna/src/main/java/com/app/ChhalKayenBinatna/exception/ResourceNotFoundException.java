d0c0ae3ed8daa847bc16a5638b059f83a6665924
KhalilFarouqi <k.farouqi@itroadgroup.com>
2023-05-15 15:55:25 +0100

    add classe of exeption

--- ChhalKayenBinatna/src/main/java/com/app/ChhalKayenBinatna/exception/ResourceNotFoundException.java
+++ ChhalKayenBinatna/src/main/java/com/app/ChhalKayenBinatna/exception/ResourceNotFoundException.java
@@ -1,2 +1,7 @@
-package com.app.ChhalKayenBinatna.exception;public class ResourceNotFoundException {
+package com.app.ChhalKayenBinatna.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
